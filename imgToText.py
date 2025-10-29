0e9c2666f150250800e7500f67740bec5a5cef2e
PRERAN S <prerans181@gmail.com>
2025-10-29 23:45:02 +0530

    Change image_path to an empty string
    Updated image path to an empty string.

--- imgToText.py
+++ imgToText.py
@@ -1,7 +1,7 @@
 import pytesseract
 from PIL import Image
 pytesseract.pytesseract.tesseract_cmd=r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
-image_path=r"C:\\Users\\preran s\\OneDrive\\Pictures\\Screenshots\\Screenshot (642).png"
+image_path=r""
 img=Image.open(image_path)
 text=pytesseract.image_to_string(img)
 print("extracted text")
